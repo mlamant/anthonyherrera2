d9dbdf4b6c0233840a47c0a01ed88471be4bf8fb
mlamant <90951330+mlamant@users.noreply.github.com>
2022-02-19 01:21:52 -0800

    Assignment 4

---
+++ fancifymytext.js
@@ -0,0 +1,28 @@
+function myFunction()
+{
+	alert("Hello, world!");
+	document.getElementById("words").style.fontSize = "24pt";
+}
+
+function functionTwo()
+{
+	document.getElementById("words").style.fontWeight = "bold";
+	document.getElementById("words").style.color = "#0000FF";
+	document.getElementById("words").style.textDecoration = "underline";
+
+}
+function functionThree()
+{
+	document.getElementById("words").style.fontWeight = "normal";
+	document.getElementById("words").style.color = "#000000";
+	document.getElementById("words").style.textDecoration = "none";
+	document.getElementById("words").style.fontSize = "initial";
+
+}
+function functionFour()
+{
+	var x = document.getElementById("words");
+	x.style.textTransform = "uppercase";
+	var parts = x.value.split(".");
+	x.value = parts.join("-Moo");
+}
